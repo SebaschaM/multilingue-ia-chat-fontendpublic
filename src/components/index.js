4b673d114df08f474972dd57a165f45059488809
Sebastián Chaquila <schaquila@autonoma.edu.pe>
2023-12-06 19:45:21 -0500

    Fix import case in LandingPart3 component

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,6 @@ export { default as MessageMe } from "./admin/chat/MessageMe";
 export { default as ModalCustom } from "./admin/modal/ModalCustom";
 export { default as TableCustom } from "./admin/table/TableCustom";
 export { default as TableCustomForUserManagment } from "./admin/table/TableCustomForUserManagment";
-export { default as LandingPart3 } from "./client/landingPart3";
+export { default as LandingPart3 } from "./client/LandingPart3";
 export { default as LandingPart2 } from "./client/LandingPart2";
 //export { default as FooterArea } from "./client/chat/FooterArea";
